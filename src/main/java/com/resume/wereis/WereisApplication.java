9b670af0c32333318d027b7bcd66e39966f34dcd
Anatolii <m0331436@gmail.com>
2023-06-08 18:06:14 +0300

    Initialize git repository

---
+++ src/main/java/com/resume/wereis/WereisApplication.java
@@ -0,0 +1,13 @@
+package com.resume.wereis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WereisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WereisApplication.class, args);
+    }
+
+}
