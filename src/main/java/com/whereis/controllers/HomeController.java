3c8b91d26a229e007f4c1b8ddc5a1a6029504a3d
Anatolii <m0331436@gmail.com>
2023-06-10 21:30:49 +0300

    DB successfully connected, data can be accessed

--- src/main/java/com/whereis/controllers/HomeController.java
+++ src/main/java/com/whereis/controllers/HomeController.java
@@ -1,14 +1,26 @@
 package com.whereis.controllers;
 
+import com.whereis.entities.Warehouse;
+import com.whereis.services.WarehouseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.List;
+
+@Controller
 @RequestMapping("/")
 public class HomeController {
-    @GetMapping("/")
-    public String helloWorld() {
-        return "Welcome to WereIs first deployment. Congrats :)";
+
+    @Autowired
+    WarehouseService warehouseService;
+
+    @GetMapping
+    public String home(Model model) {
+        List<Warehouse> warehouses = warehouseService.findAll();
+        model.addAttribute("warehouses", warehouses);
+        return "home";
     }
 }
