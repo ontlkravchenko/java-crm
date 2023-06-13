6054a40a95993e0dafe33a734ba5193a485502e9
Anatolii <m0331436@gmail.com>
2023-06-13 12:59:31 +0300

    To check if static ip for ECS really works

--- src/main/java/com/whereis/controllers/HomeController.java
+++ src/main/java/com/whereis/controllers/HomeController.java
@@ -19,6 +19,7 @@ public class HomeController {
 
     @GetMapping
     public String home(Model model) {
+        // Comment only for git commit
         List<Warehouse> warehouses = warehouseService.findAll();
         model.addAttribute("warehouses", warehouses);
         return "home";
